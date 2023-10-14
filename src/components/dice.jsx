986408290a3dc1777611f0d7ce613aaf381847f4
Ismael Barea Insua <80041279+quantosh@users.noreply.github.com>
2023-10-14 16:42:32 +0200

    stats are now a prop component

--- src/components/dice.jsx
+++ src/components/dice.jsx
@@ -1,10 +1,15 @@
 function Dice() {
 
   return (
-    <div className="bg-red-600 p-1 m-2 max-w-fit rounded-sm">
-        <picture className="flex-auto rounded-sm">
-          <img src="https://img.freepik.com/premium-vector/pixel-art-dice-bit-game-icon-white-background_360488-55.jpg" width="100px" height="" alt="Character Portrait" />
+    <div className="max-w-fit p-2 bg-white border border-gray-200 rounded-lg shadow m-1">
+    <p className="py-2 text-[8px]">
+      Dice
+    </p>
+    <div className=""></div>
+        <picture className="">
+          <img src="https://img.freepik.com/premium-vector/pixel-art-dice-bit-game-icon-white-background_360488-55.jpg" width="100px" alt="Character Portrait" />
         </picture>
+        <button type="button" className="text-xs text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-3 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Roll!</button>
     </div>
   )
 }
--- src/components/stat.jsx
+++ src/components/stat.jsx
@@ -1,28 +1,34 @@
-import bicepImage from '../assets/bicep.png';
+import bicepImage from '../assets/BICEP.png';
+import heartImage from '../assets/HP.png';
+import inteligenceImage from '../assets/INT.png';
+import brainImage from '../assets/WIS.png';
+import staminaImage from '../assets/STA.png';
+import agilityImage from '../assets/AGI.png';
 
 function Stat(props) {
   const { statType, statName, statValue } = props;
   const imageMapping = {
     'STR': bicepImage,
-    'DEX': null,
-    'CON': null,
-    'INT': null,
-    'WIS': null,
-    'CHA': null,
+    'DEX': bicepImage,
+    'CON': bicepImage,
+    'INT': inteligenceImage,
+    'WIS': brainImage,
+    'CHA': bicepImage,
+    'STA': staminaImage,
+    'AGI': agilityImage,
+    'HP': heartImage,
   };
 
   return (
-    <div className="p-2 ml-1 mt-1 max-w-sm bg-white rounded-xl shadow-md flex items-center space-x-2">
+    <div className="p-2 ml-1 mt-1 max-w-sm bg-white rounded-xl border flex items-center space-x-2">
       <div className="shrink-0">
         <img className="h-5 " src={imageMapping[statType]} alt={statName} />
       </div>
       <div>
-        <div className="text-xs font-medium text-black">{statName}</div>
-        <p className="text-slate-700 text-xs">{statValue}</p>
+        <div className="text-[10px] font-medium text-black">{statName}</div>
+        <p className="text-[8px] text-slate-700 font-thin">{statValue}</p>
       </div>
     </div>
-
-    
   )
 }
 export default Stat
