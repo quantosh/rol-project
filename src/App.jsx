998aef16bddd394e637643164ec75b0d08a37935
Victor Martinez <vimar@eg.dk>
2023-10-15 15:46:37 +0200

    Start work on lobbies screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '@components/login'
 import Dashboard from '@components/dashboard'
 import RegisterUser from './components/registerUser';
+import Lobbies from './components/lobbies';
 
 function App() {  
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/register" element={<RegisterUser />} />
+            <Route path="/lobbies" element={<Lobbies />} />
           </Routes>
         </Router>
     </div>
