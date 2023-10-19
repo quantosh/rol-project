959b0fc2aa59075705f55440aa00820962db8130
T-NAVe <65693205+T-NAVe@users.noreply.github.com>
2023-10-19 14:08:28 -0300

    Added radix modal implementation

--- src/components/layouts/lobbies/lobby.jsx
+++ src/components/layouts/lobbies/lobby.jsx
@@ -1,66 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { doc, getDoc, collection, getDocs, query, where } from 'firebase/firestore';
-import { db, auth } from '../../../main';
-import Modal from '../../molecules/modal/modal';
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { doc, getDoc, collection, getDocs, query, where } from 'firebase/firestore'
+import { db, auth } from '../../../main'
+import Modal from '../../molecules/modal/modal'
 
 const Lobby = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [lobby, setLobby] = useState(null);
-  const [userSheets, setUserSheets] = useState([]);
-  const [selectedSheet, setSelectedSheet] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [lobby, setLobby] = useState(null)
+  const [userSheets, setUserSheets] = useState([])
+  const [selectedSheet, setSelectedSheet] = useState('')
+  const [modalOpen, setModalOpen] = useState(false)
 
   useEffect(() => {
     const fetchLobby = async () => {
-      const docRef = doc(db, 'lobbies', id);
-      const docSnap = await getDoc(docRef);
+      const docRef = doc(db, 'lobbies', id)
+      const docSnap = await getDoc(docRef)
 
       if (docSnap.exists()) {
-        setLobby({ ...docSnap.data(), id: docSnap.id });
+        setLobby({ ...docSnap.data(), id: docSnap.id })
         if (!docSnap.data().sheetId) {
-          setModalOpen(true);
-          await fetchUserSheets();
+          setModalOpen(true)
+          await fetchUserSheets()
         }
       } else {
-        console.log('No such lobby!');
-        navigate('/404');
+        console.log('No such lobby!')
+        navigate('/404')
       }
-    };
+    }
 
-    fetchLobby();
-  }, [id, navigate]);
+    fetchLobby()
+  }, [id, navigate])
 
   const fetchUserSheets = async () => {
     try {
-      const user = auth.currentUser;
+      const user = auth.currentUser
       if (user) {
-        const userSheetsData = await getDocs(query(collection(db, 'sheets'), where('userId', '==', user.uid)));
-        setUserSheets(userSheetsData.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+        const userSheetsData = await getDocs(query(collection(db, 'sheets'), where('userId', '==', user.uid)))
+        setUserSheets(userSheetsData.docs.map(doc => ({ ...doc.data(), id: doc.id })))
       }
     } catch (error) {
-      console.error('Error fetching user sheets: ', error);
+      console.error('Error fetching user sheets: ', error)
     }
-  };
+  }
 
   const handleSheetSelect = async () => {
-    if (selectedSheet !== "") {
+    if (selectedSheet !== '') {
       // Logic to assign the selected sheet to the lobby and close the modal
       // Here you'd also update the lobby in Firestore to include the sheetId
-      setModalOpen(false);
+      setModalOpen(false)
     } else {
-      alert('Please select a character sheet.');
+      alert('Please select a character sheet.')
     }
-  };
+  }
 
   const createNewSheet = () => {
     // Since we can't close the modal without a selection, we navigate with the modal open
     // You might want to add logic here to handle this part better, e.g., setting a state to know a new sheet is being created
-    navigate('/new-sheet'); // Adjust the route as needed
-  };
+    navigate('/new-sheet') // Adjust the route as needed
+  }
 
-  if (!lobby) return 'Loading...'; // Or any loading spinner
+  if (!lobby) return 'Loading...' // Or any loading spinner
 
   return (
     <div>
@@ -82,7 +82,7 @@ const Lobby = () => {
           onChange={(e) => setSelectedSheet(e.target.value)}
           disabled={userSheets.length === 0} // Disable select if no sheets available
         >
-          <option value="">{userSheets.length === 0 ? "No character sheets available" : "Select a character sheet"}</option>
+          <option value="">{userSheets.length === 0 ? 'No character sheets available' : 'Select a character sheet'}</option>
           {userSheets.map(sheet => (
             <option key={sheet.id} value={sheet.id}>{sheet.name}</option>
           ))}
@@ -90,7 +90,7 @@ const Lobby = () => {
         <button onClick={createNewSheet}>Create New Sheet</button>
       </Modal>
     </div>
-  );
-};
+  )
+}
 
-export default Lobby;
+export default Lobby
--- src/components/molecules/modal/modal.jsx
+++ src/components/molecules/modal/modal.jsx
@@ -1,30 +1,46 @@
-const Modal = ({ show, title, handleClose, handleAction, textValue, setTextValue, actionButtonText, cancelButtonText, placeholder, hideTextBox }) => {
-  if (!show) {
-    return null
-  }
+import * as Dialog from '@radix-ui/react-dialog'
+import { Cross2Icon } from '@radix-ui/react-icons'
 
+const Modal = ({ show, title, handleClose, handleAction, textValue, setTextValue, actionButtonText, cancelButtonText, placeholder, hideTextBox, buttonTextContent, buttonStyle = 'btn-primary' }) => {
   return (
-        <dialog id="modal_notification" className="card bg-primary text-primary-content">
-            <div className="flex card-body gap-2">
-                <form className="dialog bg-slate-300">
-                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={handleClose}>✕</button>
-                </form>
-                <h3 className="font-bold text-lg">{title}</h3>
-                <div className="flex gap-1">
-                    {!hideTextBox && (
-                        <input
-                            type="text"
-                            value={textValue}
-                            onChange={(e) => setTextValue(e.target.value)}
-                            placeholder={placeholder}
-                            className='input input-bordered w-full max-w-xs'
-                        />
-                    )}
-                    <button className='btn btn-success' onClick={handleAction}>{actionButtonText}</button>
-                    <button className='btn' onClick={handleClose}>{cancelButtonText}</button>
-                </div>
+    <Dialog.Root open={show} modal>
+      <Dialog.Trigger asChild>
+          <button className={`btn ${buttonStyle}`}>{buttonTextContent}</button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black opacity-20 z-10 fixed inset-0" />
+          <Dialog.Content className={`flex flex-col z-20 fixed top-1/2 left-1/2 justify-center
+        translate-x-[-50%] translate-y-[-50%] shadow-md gap-2 card`}>
+            <div className='card-body bg-base-100 rounded-lg'>
+              <Dialog.Close asChild>
+                <button className="absolute right-3 top-3 rounded-full bg-slate-600 bg-opacity-25" aria-label="Close">
+                    <Cross2Icon />
+                </button>
+              </Dialog.Close>
+              <Dialog.Title className="font-bold text-lg">{title}</Dialog.Title>
+              <div className="flex gap-1">
+                  {!hideTextBox && (
+                      <input
+                          type="text"
+                          value={textValue}
+                          onChange={(e) => setTextValue(e.target.value)}
+                          placeholder={placeholder}
+                          className='input input-bordered w-full max-w-xs'
+                      />
+                  )}
+                  <Dialog.Close asChild>
+                      <button className='btn btn-success' onClick={handleAction}>{actionButtonText}</button>
+                  </Dialog.Close>
+              </div>
+              <Dialog.Close asChild>
+                  <button className='btn' onClick={handleClose}>{cancelButtonText}</button>
+              </Dialog.Close>
+              <Dialog.Close asChild>
+              </Dialog.Close>
             </div>
-        </dialog >
+          </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
 
